1ea58725a93ad0dbcbcfe5afdfab978201f3d573
Malcolm <malichaooo@gmail.com>
2017-12-30 17:56:07 -0800

    Add initial car visualization

---
+++ car.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.lines import Line2D
+from matplotlib.text import Annotation
+import math
+
+
+class Car:
+    def __init__(self, axes=None, id=0, length=5, width=2, label='car0', color='black'):
+        self.id = id
+        self.set_pose(0, 0, 0)
+        self.set_length_width(length, width)
+        self.label = Annotation('car0', [0, 0])
+        self.bbox = Line2D([], [], 2, color=color)
+        if axes:
+            axes.add_line(self.bbox)
+            axes.add_artist(self.label)
+
+    def set_length_width(self, length, width):
+        self.length = length    # x dir
+        self.width = width      # y dir
+        self._bbox_x = [0, length, length, 0, 0,length * 0.3,0]
+        self._bbox_y = [0, 0, width, width, 0,width*0.5,width]
+        for i in range(0, len(self._bbox_x)):
+            self._bbox_x[i] -= length * 0.3
+            self._bbox_y[i] -= width * 0.5
+        self.bbox_x = [[], [], [], [], [], [],[],]
+        self.bbox_y = [[], [], [], [], [], [],[],]
+
+    def set_pose(self, x, y, yaw):
+        self.x, self.y, self.yaw = x, y, yaw
+
+    def draw(self,axes=None):
+        # Update bounding box
+        for i in range(0, len(self._bbox_x)):
+            self.bbox_x[i], self.bbox_y[i] = self.local_to_global(self._bbox_x[i],
+                                                                  self._bbox_y[i])
+        self.bbox.set_data(self.bbox_x,self.bbox_y)
+
+        # Update label position
+        self.label.set_x(self.x)
+        self.label.set_y(self.y)
+        
+        # if axes:
+        #     axes.add_line(self.bbox)
+        #     axes.add_artist(self.label)
+
+    def rotate(self, x, y, yaw):
+        x_ = x * math.cos(yaw) - y * math.sin(yaw)
+        y_ = x * math.sin(yaw) + y * math.cos(yaw)
+        return x_, y_
+
+    def local_to_global(self, x, y):
+        x_, y_ = self.rotate(x, y, self.yaw)
+        return x_ + self.x, y_ + self.y
+
+    def global_to_local(self, x, y, offset, yaw):
+        x_, y_ = x - self.x, y - self.y
+        return self.rotate(x_, y_, -self.yaw)
+
+t = Annotation('car0', xy=(2, 1),xytext=(3, 1.5),color='black')
+def main():
+    print("Test Car class")
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111, aspect='equal')
+
+    car = Car(ax1)
+    car.set_pose(5, 5, math.pi/4)
+    car.draw()
+
+    ax1.set_xlim(-10, 10)
+    ax1.set_ylim(-10, 10)
+    ax1.grid(True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
